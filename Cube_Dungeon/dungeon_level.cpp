45935f823a8f757d322969f57f96f37ae97431f5
janderkkotlarski <j.d.kotlarski@gmx.com>
2020-07-29 10:59:33 +0200

    Working on level 7

--- Cube_Dungeon/dungeon_level.cpp
+++ Cube_Dungeon/dungeon_level.cpp
@@ -468,7 +468,21 @@ noexcept
   block_of_cubes(type_volume, cube_type::none, radius - 9, radius - 9, radius - 6, radius + 6, radius + 6, radius + 6);
   block_of_cubes(type_volume, cube_type::none, radius - 9, radius - 9, radius + 6, radius + 6, radius - 4, radius + 6);
 
-  type_volume[radius - 9][radius + 6][radius - 7] = cube_type::none;
+  type_volume[radius - 8][radius + 6][radius - 4] = cube_type::none;
+
+  block_of_cubes(type_volume, cube_type::none, radius - 7, radius - 7, radius - 6, radius + 6, radius - 6, radius - 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 7, radius - 7, radius - 6, radius - 6, radius - 6, radius + 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 7, radius - 7, radius - 6, radius + 6, radius + 6, radius + 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 7, radius - 7, radius + 6, radius + 6, radius - 4, radius + 6);
+
+  type_volume[radius - 6][radius + 6][radius - 6] = cube_type::none;
+
+  block_of_cubes(type_volume, cube_type::none, radius - 5, radius - 5, radius - 6, radius + 4, radius - 6, radius - 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 5, radius - 5, radius - 6, radius - 6, radius - 6, radius + 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 5, radius - 5, radius - 6, radius + 6, radius + 6, radius + 6);
+  block_of_cubes(type_volume, cube_type::none, radius - 5, radius - 5, radius + 6, radius + 6, radius - 6, radius + 6);
+
+  type_volume[radius - 4][radius + 4][radius - 6] = cube_type::none;
 }
 
 void demo_1(std::vector< std::vector <std::vector <cube_type>>> &type_volume,
